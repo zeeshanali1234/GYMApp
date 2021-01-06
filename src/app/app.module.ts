f993e90edb01663e9637c0e4e2bc58d7b025ca16
zeeshan ali <zeeshanalispn@gmail.com>
2021-01-06 14:09:01 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePage } from './home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    HeaderComponent,
+    FooterComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+selector:'HomeComponents',
+template:'<h2>Welcome<h2>'
+
+})
+export class HomePage{
+
+}
